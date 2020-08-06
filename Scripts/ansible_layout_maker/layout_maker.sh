5122b44f2614112b92a0afaa70ba86e91ce9b39f
しーたる <62013801+thetaru@users.noreply.github.com>
2020-08-07 01:02:27 +0900

    Update layout_maker.sh

--- Scripts/ansible_layout_maker/layout_maker.sh
+++ Scripts/ansible_layout_maker/layout_maker.sh
@@ -1,1 +1,51 @@
-#
+#!/bin/bash
+#######################################################################
+# confirm
+#######################################################################
+
+echo "You are in '"`pwd`"' here."
+read -p "Are you sure to make directories? (y/N): " confirm
+case "$confirm" in
+    [Yy]|[Yy][Ee][Ss])
+        echo "make directories..."
+        ;;
+    [Nn]|[Nn][Oo])
+        echo "Bye"
+        exit
+        ;;
+    *)
+        echo "Invalid input. Please input y/N."
+esac
+
+#######################################################################
+# make directories
+#######################################################################
+
+# root
+mkdir playbook
+
+# group_vars: env
+mkdir playbook/group_vars
+
+# host_vars: env
+mkdir playbook/host_vars
+
+# inventories:
+mkdir playbook/inventory
+
+# roles:
+ansible-galaxy init playbook/roles
+
+#######################################################################
+# make yml files
+#######################################################################
+
+# Main Playbook
+(
+    echo "---"
+) > playbook/site.yml
+
+# inventory:
+(
+    echo "---"
+) > playbook/inventory/hosts
