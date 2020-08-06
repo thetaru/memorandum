11a98611ee5ac58974d1a0679826aeede57f14cb
しーたる <62013801+thetaru@users.noreply.github.com>
2020-08-07 08:37:13 +0900

    Update layout_maker.sh

--- Scripts/ansible_layout_maker/layout_maker.sh
+++ Scripts/ansible_layout_maker/layout_maker.sh
@@ -51,3 +51,9 @@ ansible-galaxy init playbook/roles
     echo "#[example]"
     echo "#192.168.0.1 node_hostname=example-host"
 ) > playbook/inventory/hosts
+
+# group_vars: example-host refer to group_vars/example/example.yml
+(
+    echo "---"
+    echo "# write about ..."
+) > playbook/group_vars/example/example.yml
