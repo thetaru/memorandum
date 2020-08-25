8c2e19257d43e7f8f9ba96df5018735149bbe24a
しーたる <62013801+thetaru@users.noreply.github.com>
2020-08-25 15:11:22 +0900

    Update join-cluster.sh

--- Scripts/k8s_join_cluster/join-cluster.sh
+++ Scripts/k8s_join_cluster/join-cluster.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-read -p "Enter the IP address to join the cluster(e.g. 192.168.0.1): "
+read -p "Enter the IP address to join the cluster(e.g. 192.168.0.1): " node_ip
+read -p "Enter the node username(e.g. kube-node): " node_name
+ssh $node_name@$node_ip <<EOC
