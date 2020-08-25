14b05a95d5cd79ad88a1e755777422f12368533c
しーたる <62013801+thetaru@users.noreply.github.com>
2020-08-25 15:24:19 +0900

    Update join-cluster.sh

--- Scripts/k8s_join_cluster/join-cluster.sh
+++ Scripts/k8s_join_cluster/join-cluster.sh
@@ -4,4 +4,11 @@ read -p "Enter the IP address to join the cluster(e.g. 192.168.0.1): " node_ip
 # Get Token
 token=`kubeadm token list | awk '{ print $1 }'`
 
+# If empty
+if [[ $token -eq "" ]]; then
+  kubeadm token create
+  token=`kubeadm token list | awk '{ print $1 }'`
+fi
+
 ssh root@$node_ip <<EOC
+  kubeadm join 
