ce15fa07e154636f9923095b124823981f2479e7
しーたる <62013801+thetaru@users.noreply.github.com>
2021-04-17 19:11:34 +0900

    Update firewall.sh

--- OS/Linux/CentOS8/firewalld/firewall.sh
+++ OS/Linux/CentOS8/firewalld/firewall.sh
@@ -1,25 +1,32 @@
 #!/bin/bash
 
+### CONST
+zone=$(firewall-cmd --get-default-zone)
+
 ### INIT
 ## ports
 list_ports=$(firewall-cmd --list-ports)
 for port in ${list_ports[@]}; do
-  firewall-cmd --remove-port=${port}
+  firewall-cmd --permanent --zone=${zone} --remove-port=${port}
 done
 
 ## rich rule
 OLD_IFS=$IFS
 IFS=$'\n'
-list_rules=$(firewall-cmd --zone=public --list-rich-rules)
-for rule in ${list_rules[@]}; do
-  firewall-cmd --permanent --zone=public --remove-rich-rule="${rule}"
+list_rich_rules=$(firewall-cmd --list-rich-rules)
+for rule in ${list_rich_rules[@]}; do
+  firewall-cmd --permanent --zone=${zone} --remove-rich-rule="${rule}"
 done
 IFS=$OLF_IFS
 
 ## direct rule
-
-
-
+OLD_IFS=$IFS
+IFS=$'\n'
+list_direct_rules=$(firewall-cmd --direct --get-all-rules)
+for rule in ${list_direct_rules[@]}; do
+  firewall-cmd --permanent --direct --remove-rule "${rule}"
+done
+IFS=$OLF_IFS
 
 ### reload
 firewall-cmd --reload
