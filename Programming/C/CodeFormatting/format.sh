cd0855c0f96b1ac7d0ba8cef327e5eebfe1311a2
しーたる <62013801+thetaru@users.noreply.github.com>
2021-08-28 20:40:20 +0900

    Update format.sh

--- Programming/C/CodeFormatting/format.sh
+++ Programming/C/CodeFormatting/format.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+if [[ $# -gt 1 ]]; then
+  echo "Syntax error." 1>&2
+  echo "usage: [bash] format.sh [DIRECTORY]" 1>&2
+  echo "" 1>&2
+  exit 1
+fi
+
 if [[ ! -d "${1:-$(pwd)}" ]]; then
+  echo "First argument is not Directory." 1>&2
   echo "usage: [bash] format.sh [DIRECTORY]" 1>&2
   exit 1
 fi
