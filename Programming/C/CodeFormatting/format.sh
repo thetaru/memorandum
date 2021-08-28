8699df3dd5449ed639ce68250a33d7547daa97b0
しーたる <62013801+thetaru@users.noreply.github.com>
2021-08-29 01:02:33 +0900

    Update format.sh

--- Programming/C/CodeFormatting/format.sh
+++ Programming/C/CodeFormatting/format.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 set -eu
 
+PWD=$(cd $(dirname $0);pwd)
+
 if [ $# -gt 1 ]; then
   echo "Syntax error." 1>&2
   echo "usage: [bash] format.sh [DIRECTORY]" 1>&2
-  echo "" 1>&2
   exit 1
 fi
 
-if [ ! -d "${1:-$(pwd)}" ]; then
+if [ ! -d "${1:-${PWD}}" ]; then
   echo "First argument is not Directory." 1>&2
   echo "usage: [bash] format.sh [DIRECTORY]" 1>&2
   exit 1
 fi
 
+if [ ! -f "${1:-${PWD}}/.clang-format" ]; then
+  echo ".clang-format file not found." 1>&2
+  exit 1
+fi
+
 echo "Formatting Code..."
-clang-format -i -style=file "${1:-$(pwd)}"/*.c
-clang-format -i -style=file "${1:-$(pwd)}"/*.h
+if ls "${1:-${PWD}}"/*.c > /dev/null 2>&1; then
+  clang-format -i -style=file "${1:-${PWD}}"/*.c
+fi
+
+if ls "${1:-${PWD}}"/*.h > /dev/null 2>&1; then
+  clang-format -i -style=file "${1:-${PWD}}"/*.h
+fi
 echo "Done"
+
 exit 0
