fc1bcfb06c1527c63d9c080147c7b23d030e550c
しーたる <62013801+thetaru@users.noreply.github.com>
2021-08-28 20:24:30 +0900

    Update format.sh

--- Programming/C/CodeFormatting/format.sh
+++ Programming/C/CodeFormatting/format.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-if [[ $# -ne 1 ]]; then
-  echo "usage: format.sh DIRECTORY" 1>&2
-  exit 1
-fi
-
-if [[ ! -d "$1" ]]; then
-  echo "usage: format.sh DIRECTORY" 1>&2
+if [[ ! -d "${1:-$(pwd)}" ]]; then
+  echo "usage: [bash] format.sh [DIRECTORY]" 1>&2
   exit 1
 fi
 
 echo "Formatting Code..."
-clang-format -i -style=file "$1"/*.c
-clang-format -i -style=file "$1"/*.h
+clang-format -i -style=file "${1:-$(pwd)}"/*.c
+clang-format -i -style=file "${1:-$(pwd)}"/*.h
 echo "Done"
 exit 0
