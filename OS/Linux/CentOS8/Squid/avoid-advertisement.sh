caf890efdafdcfddeccad139cb974ed5258b182f
しーたる <62013801+thetaru@users.noreply.github.com>
2021-09-08 14:34:44 +0900

    Create avoid-advertisement.sh

---
+++ OS/Linux/CentOS8/Squid/avoid-advertisement.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+OUTPUT="/etc/squid/blacklists/common"
+LOGFILE="/var/log/squid/UrlHealthCheck.log"
+
+URLS=()
+URLS+=("https://adaway.org/hosts.txt")
+URLS+=("http://winhelp2002.mvps.org/hosts.txt")
+URLS+=("https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&showintro=0&mimetype=plaintext")
+URLS+=("https://raw.githubusercontent.com/multiverse2011/adawaylist-jp/master/hosts")
+URLS+=("https://warui.intaa.net/adhosts/hosts.txt")
+
+for URL in "${URLS[@]}"; do
+  # GET HTTP STATUS
+  HTTP_STATUS=$(curl -LI "$URL" -o /dev/null -w '%{http_code}\n' -s)
+
+  if [ -f "${OUTPUT}" ]; then
+    cp -p "${OUTPUT}" "${OUTPUT}.$(date +%Y%m%d)"
+    rm -f "${OUTPUT}"
+  fi
+
+  # GET BLACKLIST
+  if [ "$HTTP_STATUS" -eq "200" ]; then
+    curl -sfL "$URL" | \
+         grep -v \
+              -e localhost \
+              -e ^\# \
+              -e ^$ | \
+         sed  -e 's/^[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}//g' \
+              -e 's/^ *\| *$//' >> "${OUTPUT}"
+  else
+    echo "[$(date +'%Y-%m-%d %H:%M:%S')] ERROR: Failed to access url ${URL}" >> "${LOGFILE}"
+  fi
+done
+
+sort -u "${OUTPUT}" -o "${OUTPUT}"
