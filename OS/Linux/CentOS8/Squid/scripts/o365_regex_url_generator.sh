fa63ceb51e47d56eb0cf2b15cfca03148769a6f4
しーたる <62013801+thetaru@users.noreply.github.com>
2021-12-10 02:44:36 +0900

    Update o365_regex_url_generator.sh

--- OS/Linux/CentOS8/Squid/scripts/o365_regex_url_generator.sh
+++ OS/Linux/CentOS8/Squid/scripts/o365_regex_url_generator.sh
@@ -30,7 +30,7 @@ function generate_regex_url() {
 function get_url_list() {
   # Office 365 URL および IP アドレス範囲
   # https://docs.microsoft.com/ja-jp/microsoft-365/enterprise/urls-and-ip-address-ranges?view=o365-worldwide
-  local readonly URL="https://endpoints.office.com/endpoints/worldwide?clientrequestid=b10c5ed1-bad1-445f-b386-b919946339a7"
+  local -r URL="https://endpoints.office.com/endpoints/worldwide?clientrequestid=b10c5ed1-bad1-445f-b386-b919946339a7"
   local URL_LIST=$(curl -s ${URL} | jq -r '.[] | select(.serviceAreaDisplayName == "Microsoft 365 Common and Office Online") | select(.urls != null) |.urls[]' | sort -u)
   echo ${URL_LIST}
 }
